82f8e4a144c5fd33dc72c31a9bd29b171af84a7c
JokerYan <770792058@qq.com>
2020-12-06 12:40:57 +0800

    Add gpu device

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import argparse
 from torchvision import datasets
 from torchvision.transforms import transforms
 
@@ -10,11 +11,22 @@ from model import Net
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='PyTorch Example')
+    parser.add_argument('--disable-cuda', action='store_true',
+                        help='Disable CUDA')
+    args = parser.parse_args()
+    args.device = None
+
+    if not args.disable_cuda and torch.cuda.is_available():
+        args.device = torch.device('cuda')
+    else:
+        args.device = torch.device('cpu')
+
     surface_train_dataset = SurfaceDataset(csv_file=os.path.join('Data', 'CombinedOutputs', 'csv', 'all_data.csv'),
                                            root_dir='Data')
     dataloader = DataLoader(surface_train_dataset, batch_size=4,
                             shuffle=True, num_workers=0)
-    model = Net()
+    model = Net().to(args.device)
 
     criterion = torch.nn.MSELoss(reduction='sum')
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
@@ -23,8 +35,8 @@ if __name__ == "__main__":
         running_loss = 0.0
 
         for i, data in enumerate(dataloader):
-            inputs = data['image']
-            labels = data['direction']
+            inputs = data['image'].to(args.device)
+            labels = data['direction'].to(args.device)
             optimizer.zero_grad()
 
             outputs = model(inputs)
